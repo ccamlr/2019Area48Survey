883825a59297818d56f9d4f63b4c259b07f3f4de
gavinmacaulay <gavin@macaulay.co.nz>
2019-08-08 21:26:51 +0200

    Output the results in a format suitable for copy/paste into a word table

--- src/do_all.m
+++ src/do_all.m
@@ -27,9 +27,8 @@ do_calculate_ts
 %% Calculate the biomass
 do_estimate_biomass
 
-%% Show the biomass results formatted to make it easy to copy/paste into the ASAM report
+%% Show the biomass results 
 do_define_directories
-
 load(fullfile(resultsDir, 'Final results'), 'results')
 
 % Show the per strata results
@@ -40,3 +39,25 @@ disp(results.biomass_strata)
 disp('Results per survey:')
 disp(results.biomass_survey)
 
+
+% Do it to make it easy to copy/paste an appropriately formatted
+% table into the ASAM report (via Excel)
+disp('strata for pasting:')
+
+for i = 1:height(results.biomass_strata)
+    r = results.biomass_strata(i,2:5);
+    fprintf('%s; %.1f; %s; %s,000\n', jf.format(r.area), r.meanDensity, ...
+        jf.format(round(r.biomass/1e3)*1e3), ...
+        jf.format(round(r.varianceComponent/1e9)))
+end
+
+% surveys
+disp('surveys for pasting')
+for i = 1:height(results.biomass_survey)
+    r = results.biomass_survey(i,2:7);
+    fprintf('%.1f; %.1f; %.0f; %s; %s,000; %.0f\n', r.meanDensity, ...
+        r.meanDensityVariance, r.meanDensityCV, ...
+        jf.format(round(r.biomass/1e3)*1e3), ...
+        jf.format(round(r.variance/1e9)), ...
+        r.CV)
+end
