eab11ac675e338cd3ffe78b8c315da3c27561817
Gavin Macaulay <gavin@macaulay.co.nz>
2019-08-27 10:09:18 +0200

    sigh... usb drive changed letter on me...

--- src/do_define_directories.m
+++ src/do_define_directories.m
@@ -1,5 +1,5 @@
 %%
 % COmmon directories that all the other scripts use
-baseDir = 'I:\KRILL2019';
+baseDir = 'J:\KRILL2019';
 repoDir = '2019Area48SurveyRepo';
 resultsDir = fullfile(baseDir, repoDir, 'results');
